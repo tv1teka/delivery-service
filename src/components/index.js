8690b17958fb50d26f4568f9736623bb69c7739b
tv1teka <solyanik.vtr@gmail.com>
2021-12-01 12:53:12 +0300

    sort popup

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
 import Header from './Header'
 import Button from './Button'
 import Categories from './Categories'
+import SortPopup from './SortPopup';
 
-export {Header, Button, Categories}
+export {Header, Button, Categories, SortPopup}
