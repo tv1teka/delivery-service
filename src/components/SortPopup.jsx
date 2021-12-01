a3088db1583f3157f7d018641509aee22e6f67db
tv1teka <solyanik.vtr@gmail.com>
2021-12-01 12:58:33 +0300

    sort svg rotate

--- src/components/SortPopup.jsx
+++ src/components/SortPopup.jsx
@@ -33,6 +33,7 @@ function SortPopup({items, onClickItem}) {
         className="sort">
         <div className="sort__label">
           <svg
+            className={visiblePopup ? 'rotated': ''}
             width="10"
             height="6"
             viewBox="0 0 10 6"
